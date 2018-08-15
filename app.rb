89214b7034b77db6e0e929a9933050a59b84001f
frisaac <frisaac@stu.naperville203.org>
2018-08-15 15:57:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get 'reversename/ :name'(name) do
+    letters = []
+    
+    num.each do |x|
+      letters << x
+    end
+    x = x.size
+    while x != 0
+      x += -1
+      puts x
+    end
+  end
 end
